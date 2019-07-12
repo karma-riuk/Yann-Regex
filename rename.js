d5a2cc2b7ef7353b9996903d9a4b815f83f36464
Karma Riuk <riukkarma@gmail.com>
2019-07-12 23:02:12 +0200

    Finished rename

--- rename.js
+++ rename.js
@@ -29,13 +29,57 @@
 /*
  * TEST AREA
  */
-theFile.rename("coucou.pdf") // if theFile is a File
 
-theFile = "coucou.pdf" // maybe it can work, idk
+//theFile.rename("coucou.pdf") // if theFile is a File
+
+//theFile = "coucou.pdf" // maybe it can work, idk
 
 /*
  * Final code (in function of what the test gave)
  */
 
-new RegExp('\\d{1,3}([- ])([iI]([abAB])|[iI]?[vV]|[iI]{0,3})([- ])\\d*')).test(theFile) // if theFile is a String
+
+
+
+
+
+function rename(theFile){
+    var re  = new RegExp('(\\d{1,3})([- ])([iI][abAB]|[iI]?[vV]|[iI]{0,3})([- ])(\\d*)'); //initialize the regex
+
+    var matches = theFile.match(re); // get the groups of the regex given the filename, [0] is the match itself, form 1 till the end are the groups
+
+
+    var newFile = matches[1]; //the new filename that will always start with the first group (the first 1-3 numbers)
+
+    newFile += " "; // there will always be a space after the first group, as aksed
+
+    var reAB = /[abAB]/;
+
+    var current = matches[3];
+
+    if (reAB.test(current)) 
+        newFile += "I" + current.match(reAB)[0].toLowerCase();// if there is a [abAB], it can only start with an I (as said buy the employer)
+    else
+        newFile += current.toUpperCase(); // since there is no [abAB], it must go uppercase as it is
+
+    newFile += " " + matches[5]; // always end with with last numbers, since they don't have to be modified :)
+
+    return newFile;
+}
+
+
+//var theFile = "109-IA 1"; // mock filename
+var theFile = ["144-iii-344", "132-i-11", "110-ib-351", "109-ia-1"]; // mock filename
+
+for (var name in theFile){
+    print("Input:  "+ theFile[name]);
+    print("Output: "+ rename(theFile[name]));
+    print()
+}
+
+
+
+
+
+
 
